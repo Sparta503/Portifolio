84dd97d3b8c9c667d64cd1b1b30da78bbe33a266
DiorHomme <chomunodisaa68@gmail.com>
2026-03-24 00:02:39 +0200

    Update Home.vue

--- src/components/Views/Home.vue
+++ src/components/Views/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <Hero />
+    <HomeHero />
     <About />
     <Projects />
+    <Skills />
+    <Experience />
+    <Education />
     <Contact />
   </div>
 </template>
 
 <script setup>
-import Hero from '../sections/Hero.vue'
+import HomeHero from '../sections/HomeHero.vue'
 import About from '../sections/About.vue'
 import Projects from '../sections/Projects.vue'
+import Skills from '../sections/Skills.vue'
+import Experience from '../sections/Experience.vue'
+import Education from  '../sections/Education.vue'
 import Contact from '../sections/Contact.vue'
 </script>
