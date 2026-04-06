ea1896d596c263ceedd47d5069d65a4a74f7f60d
DiorHomme <chomunodisaa68@gmail.com>
2026-04-07 00:54:25 +0200

    Update Skills.vue

--- src/components/sections/Skills.vue
+++ src/components/sections/Skills.vue
@@ -8,7 +8,7 @@
 
         <p class="text-blue-100 leading-relaxed mb-4">
           I specialize in designing user experiences, crafting engaging interfaces,
-          and building robust web applications that deliver value and usability.
+          and building robust web and mobile applications that deliver value and usability.
         </p>
 
         <p class="text-blue-100 leading-relaxed mb-6">
@@ -16,16 +16,18 @@
           solutions that are visually appealing and highly functional.
         </p>
 
-        <button class="bg-purple-500 hover:bg-purple-600 px-6 py-3 rounded-lg shadow-lg transition">
-          Say Hello!
-        </button>
+        <a href="https://api.whatsapp.com/send?phone=263776452802&text=Hello" target="_blank">
+          <button class="bg-purple-500 hover:bg-purple-600 px-6 py-3 rounded-lg shadow-lg transition">
+            Say Hello!
+          </button>
+        </a>
       </div>
 
       <!-- RIGHT SIDE (STACKED CARDS) -->
       <div class="space-y-6">
 
         <!-- CARD -->
-        <div class="bg-white text-gray-800 p-6 rounded-xl shadow-md border-l-4 border-purple-500">
+        <div class="bg-white text-gray-800 p-6 rounded-xl shadow-md border-l-4 border-purple-500 hover:-translate-y-1 hover:shadow-lg transition-all duration-200">
           <h3 class="font-semibold text-lg mb-2">User Experience (UX)</h3>
           <p class="text-sm text-gray-600">
             I design intuitive and enjoyable experiences by understanding user needs,
@@ -34,7 +36,7 @@
         </div>
 
         <!-- CARD -->
-        <div class="bg-white text-gray-800 p-6 rounded-xl shadow-md">
+        <div class="bg-white text-gray-800 p-6 rounded-xl shadow-md border-l-4 border-purple-500 hover:-translate-y-1 hover:shadow-lg transition-all duration-200">
           <h3 class="font-semibold text-lg mb-2">User Interface (UI)</h3>
           <p class="text-sm text-gray-600">
             I craft visually appealing and consistent interfaces, focusing on layout,
@@ -43,10 +45,10 @@
         </div>
 
         <!-- CARD -->
-        <div class="bg-white text-gray-800 p-6 rounded-xl shadow-md">
-          <h3 class="font-semibold text-lg mb-2">Web Development</h3>
+        <div class="bg-white text-gray-800 p-6 rounded-xl shadow-md border-l-4 border-purple-500 hover:-translate-y-1 hover:shadow-lg transition-all duration-200">
+          <h3 class="font-semibold text-lg mb-2">Web and Mobile Development</h3>
           <p class="text-sm text-gray-600">
-            I build responsive and high-performance web applications using modern
+            I build responsive and high-performance web and mobile applications using modern
             technologies, ensuring scalability and maintainability.
           </p>
         </div>
